c6b091498984fe1cd3d97c160c5ede42a7c496e3
Emma Paris <autintristan@outlook.fr>
2021-02-04 16:43:27 +0100

    hook

--- src/classes/TrainedPokemon.jsx
+++ src/classes/TrainedPokemon.jsx
@@ -2,40 +2,53 @@ import React, { Component } from 'react';
 
 class TrainedPokemon extends Component {
   constructor() {
-    super(); // nécessaire pour l'héritage de classe
+    super();
 
-    // initialisation du state
     this.state = {
       exp: 0,
+      nickname: '',
     };
+
+    this.displayName = this.displayName.bind(this);
+    this.updateNickname = this.updateNickname.bind(this);
     this.gainExp = this.gainExp.bind(this);
   }
 
   gainExp() {
-    const { exp } = this.state;
+    // this.setState({
+    //   exp: exp + 10,
+    // });
 
-    console.log('EXP', exp);
+    this.setState(state => ({ exp: state.exp + 10 }));
+  }
 
+  updateNickname(e) {
     this.setState({
-      exp: exp + 10,
-    })
+      nickname: e.target.value,
+    });
+  }
 
+  displayName() {
+    const { name } = this.props;
+    console.log('Je suis', name);
   }
 
   render() {
     const { name, weight, src } = this.props;
-    const { exp } = this.state;
-
-    function displayName() {
-      console.log('Je suis', name);
-    }
+    const { exp, nickname } = this.state;
 
     return (
       <li className="TrainedPokemon" onMouseMove={this.gainExp}>
         <div className="name">{name}</div>
+        <input
+          value={nickname}
+          className="nickname"
+          onChange={this.updateNickname}
+          placeholder="Entrez un surnom"
+        />
+        <div className="exp">{exp}</div>
         <div className="weight">{weight} kg</div>
         {src && <img src={src} alt={name} />}
-        <div className='exp'>{exp}</div>
       </li>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,5 @@ import App from './classes/App';
 
 import './index.scss';
 
-import data from './_data/pokemon.json';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-console.log('DATA', data);
-
-ReactDOM.render(<App data={data} />, document.getElementById('root'));
